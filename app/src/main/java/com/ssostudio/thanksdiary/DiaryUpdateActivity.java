536e982dfb1736a318bc6a9c5bd62abb3a3a5f84
sojaehong <woghd4545@gmail.com>
2020-10-14 14:34:25 +0900

    201014
    - 일기 수정 기능 작업중
    - 일기 수정 레이아웃 작업 완료
    - 일기 내용 작성 다이얼로그 레이아웃 수정
    
    * TODO
    - 백업 복구 기능 수정 필요
    - 프리미엄 업그레이드시 셋팅의 업그레이드 부분에 버튼 없애고 프리미엄 유저라는 표시 필요
    - 일기 수정 기능 필요
    - 로컬 언어 일본어 추가 필요

--- app/src/main/java/com/ssostudio/thanksdiary/DiaryUpdateActivity.java
+++ app/src/main/java/com/ssostudio/thanksdiary/DiaryUpdateActivity.java
@@ -1,17 +1,24 @@
 package com.ssostudio.thanksdiary;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatTextView;
 
 import com.google.android.material.button.MaterialButton;
 import com.ssostudio.thanksdiary.model.DiaryModel;
+import com.ssostudio.thanksdiary.utility.DateManager;
 
 public class DiaryUpdateActivity extends AppCompatActivity implements View.OnClickListener {
     private MaterialButton backBtn, completedBtn;
     private DiaryModel _diaryModel;
+    private AppCompatTextView dateTextView, targetTextView;
+    private LinearLayout dateUpdateLayout;
+    private TextView whyTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +29,38 @@ public class DiaryUpdateActivity extends AppCompatActivity implements View.OnCli
 
     private void init(){
         getIntentData();
+        setDateTextView();
+        setTargetTextView();
+        setWhyTextView();
+        setDateUpdateLayout();
         setBackBtn();
         setCompletedBtn();
+    }
+
+    private void setWhyTextView() {
+        whyTextView = findViewById(R.id.why_a_text);
+        whyTextView.setText(_diaryModel.getDiary_content());
+    }
+
+    private void setDateUpdateLayout() {
+        dateUpdateLayout = findViewById(R.id.date_update_ll);
+        dateUpdateLayout.setOnClickListener(this);
+    }
+
+    private void setTargetTextView() {
+        targetTextView = findViewById(R.id.target_a_text);
+        targetTextView.setText(_diaryModel.getDiary_target());
+    }
+
+    private void setDateTextView() {
+        int year = _diaryModel.getDiary_year();
+        int month = _diaryModel.getDiary_month();
+        int day = _diaryModel.getDiary_day();
+
+        int[] date = {year, month, day};
+
+        dateTextView = findViewById(R.id.date_text);
+        dateTextView.setText(DateManager.dateTimeZoneFullFormat(date));
     }
 
     private void getIntentData(){
@@ -51,7 +88,14 @@ public class DiaryUpdateActivity extends AppCompatActivity implements View.OnCli
             case R.id.completed_button:
                 onCompletedBtnClick();
                 break;
+            case R.id.date_update_ll:
+                onDateUpdateLayoutClick();
+                break;
         }
+    }
+
+    private void onDateUpdateLayoutClick() {
+        finish();
     }
 
     private void onCompletedBtnClick() {
